14e8dda772d6306ff3dc4dc6e31b587740991b0b
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-04-29 22:09:15 -0600

    Mostrar bebidas

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -7,8 +7,22 @@ type DrinkCardProps = {
 
 export const DrinkCard = ({drink} : DrinkCardProps) => {
   return (
-    <div>
-        <h2 className="">{drink.strDrink}</h2>
+    <div className="shadow-lg">
+        <div className="overflow-hidden">
+            <img 
+                src={drink.strDrinkThumb} 
+                alt={`Imagen de ${drink.strDrink}`}
+                className="hover:scale-125 transition-transform hover:rotate-2"
+            />
+        </div>
+
+        <div className="p-5">
+            <h2 className="text-2xl truncate font-black">{drink.strDrink}</h2>
+            <button
+                type="button"
+                className="bg-orange-400 hover:bg-orange-500 mt-5 w-full p-3 font-bold text-white text-lg rounded-lg" 
+            > Ver receta</button>
+        </div>
     </div>
   )
 }
